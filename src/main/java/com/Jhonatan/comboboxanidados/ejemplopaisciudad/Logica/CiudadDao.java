1f89483bd207e0010ac5efd24e7b07b986932bb8
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-08 17:28:56 -0500

    class Ciudad-DAO: Metodo para listar en una tabla segun un item seleccionado

--- src/main/java/com/Jhonatan/comboboxanidados/ejemplopaisciudad/Logica/CiudadDao.java
+++ src/main/java/com/Jhonatan/comboboxanidados/ejemplopaisciudad/Logica/CiudadDao.java
@@ -3,9 +3,12 @@ package com.Jhonatan.comboboxanidados.ejemplopaisciudad.Logica;
 import com.Jhonatan.comboboxanidados.ejemplopaisciudad.Persistencia.Ciudad;
 import com.Jhonatan.comboboxanidados.ejemplopaisciudad.Persistencia.CiudadJpaController;
 import java.util.Iterator;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.swing.JComboBox;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class CiudadDao {
 
@@ -25,6 +28,27 @@ public class CiudadDao {
             }
         } catch (Exception e) {
             System.out.println("error al cargar ciudad -> " + e.getMessage());
+        }
+    }
+
+    public void listarCiudad(JTable tblCiudad, int idPais) {
+        EntityManager em = ciudadJpaController.getEntityManager();
+        DefaultTableModel model = null;
+        String[] colunmas = {"Id Ciudad", "Ciudad"};
+        model = new DefaultTableModel(null, colunmas);
+        try {
+            Query query = em.createQuery("SELECT c FROM Ciudad c JOIN  c.idPais p  WHERE p.idPais = :idPais ");
+            query.setParameter("idPais", idPais);
+            List<Ciudad> listaCiudad = query.getResultList();
+            Object filas[] = new Object[2];
+            for (Ciudad ciudad : listaCiudad) {
+                filas[0] = ciudad.getIdCiudad();
+                filas[1] = ciudad.getNombre();
+                model.addRow(filas);
+            }
+            tblCiudad.setModel(model);
+        } catch (Exception e) {
+            System.out.println("ocurrio un error em  listarCiudad: " + e.getMessage());
         }
     }
 }
