46fac55cd206c8bb8fce70ad2879062ffc6af4ba
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-07 22:04:21 -0500

    clase main: clase donde llamamos al formulario y se haga visible

--- src/main/java/com/Jhonatan/comboboxanidados/ejemplopaisciudad/ComboBoxAnidadosEjemploPaisCiudad.java
+++ src/main/java/com/Jhonatan/comboboxanidados/ejemplopaisciudad/ComboBoxAnidadosEjemploPaisCiudad.java
@@ -1,8 +1,16 @@
 package com.Jhonatan.comboboxanidados.ejemplopaisciudad;
 
-public class ComboBoxAnidadosEjemploPaisCiudad {
+import com.Jhonatan.comboboxanidados.ejemplopaisciudad.iu.frmComboAninado;
 
+public class ComboBoxAnidadosEjemploPaisCiudad {
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        llamarFormulario();
+    }
+    
+    public static void llamarFormulario() {
+        frmComboAninado de = new frmComboAninado();
+        de.setVisible(true);
     }
 }
